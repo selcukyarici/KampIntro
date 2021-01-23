83590a2d6737a408eae65dcc9b9ac8493f491a46
Selçuk Yarıcı <selcukyarici@hotmail.com>
2021-01-23 23:51:54 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Selçuk";
+            musteri1.Soyadi = "Yarıcı";
+            musteri1.MusteriNo = "12345";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "12543";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "9087654321";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
